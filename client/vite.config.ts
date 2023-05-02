442e58ca983ad6ee1c346cf9180b61176180f2b7
kimsh322 <kimsh91322@gmail.com>
2023-05-02 16:15:55 +0900

    Fix : tsconfig 파일 수정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,10 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react(), tsconfigPaths()],
-   root: "src",
-   build: {
-      outDir: "../dist",
-   },
 });
 // import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react";
