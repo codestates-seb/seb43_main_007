6e0aaf7319a5d192606e8caec28581de6d39a4d1
kimsh322 <kimsh91322@gmail.com>
2023-05-02 15:45:49 +0900

    Init : 프론트 초기세팅

---
+++ client/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+   plugins: [react(), tsconfigPaths()],
+   root: "src",
+   build: {
+      outDir: "../dist",
+   },
+});
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// const outDir = "public";
+
+// export default defineConfig({
+//    plugins: [react()],
+//    build: {
+//       assetsDir: path.relative("src", outDir),
+//    },
+// });
