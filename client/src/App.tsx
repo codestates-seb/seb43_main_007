2969bb969244b3885cd8590cc6355d3062be82c7
kimsh322 <kimsh91322@gmail.com>
2023-05-05 17:15:08 +0900

    Feat : Home 화면 레이아웃 구성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,27 @@
-import { Button } from "@mantine/core";
 import { BrowserRouter } from "react-router-dom";
+import styled from "styled-components";
 import GlobalStyles from "./style/GlobalStyle";
+import Home from "./pages/Home";
 
 function App() {
    return (
-      <BrowserRouter>
-         <GlobalStyles />
-         <Button>Click me!</Button>
-      </BrowserRouter>
+      <Container>
+         <BrowserRouter>
+            <GlobalStyles />
+            <div className="navbar">Navbar자리</div>
+            <Home />
+         </BrowserRouter>
+      </Container>
    );
 }
 
 export default App;
+
+const Container = styled.div`
+   display: flex;
+   .navbar {
+      background-color: skyblue;
+      width: 300px;
+      height: 100vh;
+   }
+`;
