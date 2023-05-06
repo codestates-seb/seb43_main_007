f6ea31b3005955aa586d4767d982ef31a5ecb158
kimsh322 <kimsh91322@gmail.com>
2023-05-06 22:38:05 +0900

    Fix : App.tsx 기존 형식으로 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
-import styled from "styled-components";
 import GlobalStyles from "./style/GlobalStyle";
 import Home from "./pages/Home";
 
 function App() {
    return (
-      <Container>
-         <BrowserRouter>
-            <GlobalStyles />
-            <div className="navbar">Navbar자리</div>
-            <Home />
-         </BrowserRouter>
-      </Container>
+      <BrowserRouter>
+         <GlobalStyles />
+         <Home />
+      </BrowserRouter>
    );
 }
 
 export default App;
-
-const Container = styled.div`
-   display: flex;
-   .navbar {
-      background-color: skyblue;
-      width: 300px;
-      height: 100vh;
-   }
-`;
