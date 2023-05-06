020e42110b972b99bd8171f6f325365168de711a
kimsh322 <kimsh91322@gmail.com>
2023-05-06 17:50:52 +0900

    Feat : footer 레이아웃 완성

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 import logo from "../../../public/img/logo1.png";
 import Introduce from "./Introduce";
+import TeamInfo from "./TeamInfo";
 
 function Footer() {
    return (
       <FooterContainer>
          <img src={logo} className="logo" alt="logo" />
          <Introduce />
-         <div>팀</div>
+         <TeamInfo />
       </FooterContainer>
    );
 }
@@ -18,6 +19,8 @@ const FooterContainer = styled.div`
    display: flex;
    background-color: #edc6b1;
    height: 200px;
+   padding: 0 50px;
+   color: var(--third-color4);
    .logo {
       width: 220px;
    }
--- client/src/components/footer/Introduce.tsx
+++ client/src/components/footer/Introduce.tsx
@@ -13,7 +13,7 @@ function Introduce() {
          <div className="intro-box">
             <span className="intro-title">Front-End</span>
             <div className="indivi-box">
-               <IndividualIntro name="주효진" img={icon1} />
+               <IndividualIntro name="주효진(팀장)" img={icon1} />
                <IndividualIntro name="이세영" img={icon2} />
                <IndividualIntro name="김수현" img={icon3} />
             </div>
@@ -21,7 +21,7 @@ function Introduce() {
          <div className="intro-box">
             <span className="intro-title">Back-End</span>
             <div className="indivi-box">
-               <IndividualIntro name="천찬웅" img={icon4} />
+               <IndividualIntro name="천찬웅(팀장)" img={icon4} />
                <IndividualIntro name="이인건" img={icon5} />
                <IndividualIntro name="김윤지" img={icon6} />
             </div>
@@ -34,16 +34,17 @@ export default Introduce;
 
 const IntroduceContainer = styled.div`
    display: flex;
-   width: 1000px;
+   width: 840px;
    .intro-box {
       display: flex;
       flex-direction: column;
       justify-content: center;
       width: 390px;
+      margin: 0 15px;
       .intro-title {
          font-size: 25px;
          text-align: center;
-         margin-bottom: 10px;
+         margin-bottom: 20px;
       }
       .indivi-box {
          display: flex;
