74de3bc87540eb7f9efdf9c5e006f39c9eb8435e
kimsh322 <kimsh91322@gmail.com>
2023-05-06 22:36:24 +0900

    Fix : App.tsx 파일 기존 형식으로 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,16 @@
 import { Button } from "@mantine/core";
 import { BrowserRouter } from "react-router-dom";
-import styled from "styled-components";
 import GlobalStyles from "./style/GlobalStyle";
 import Footer from "./components/footer/Footer";
 
 function App() {
    return (
-      <Container>
-         <BrowserRouter>
-            <GlobalStyles />
-            <Button>Click me!</Button>
-            <Footer />
-         </BrowserRouter>
-      </Container>
+      <BrowserRouter>
+         <GlobalStyles />
+         <Button>Click me!</Button>
+         <Footer />
+      </BrowserRouter>
    );
 }
 
 export default App;
-
-const Container = styled.div`
-   display: flex;
-   flex-direction: column;
-   width: 100%;
-   height: 100vh;
-`;
