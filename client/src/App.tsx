126bf9c6508d3f4bbf5a5a192c0facfc7884d7a4
kimsh322 <kimsh91322@gmail.com>
2023-05-10 10:06:58 +0900

    Fix : App.tsx 원래대로 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,22 +4,20 @@ import GlobalStyles from "./style/GlobalStyle";
 import Home from "./pages/Home";
 import Footer from "./components/footer/Footer";
 import NavbarSearch from "./components/NavBar";
-import Login from "./pages/Login";
 
 function App() {
    return (
       <Container>
          <BrowserRouter>
             <GlobalStyles />
-            {/* <div className="nav">
+            <div className="nav">
                <NavbarSearch />
                <Home />
             </div>
 
             <div className="footer">
                <Footer />
-            </div> */}
-            <Login />
+            </div>
          </BrowserRouter>
       </Container>
    );
