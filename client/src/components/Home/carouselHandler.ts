11f17bc7568abbeddfaf3a9092e56d9d35fb6636
kimsh322 <kimsh91322@gmail.com>
2023-05-13 18:01:11 +0900

    Feat : 캐러셀 완성

---
+++ client/src/components/Home/carouselHandler.ts
@@ -0,0 +1,48 @@
+import { ArgsArr } from "./homeTypes";
+
+interface PageHandler {
+   (argsArr: ArgsArr): void;
+}
+
+export const prevHandler: PageHandler = (argsArr) => {
+   const [pickNumber, setPickNumber, setCarouselTransition, setDisabled] =
+      argsArr;
+   const curNumber = pickNumber;
+   setDisabled(true);
+   if (curNumber <= 1) {
+      setPickNumber(0);
+      setTimeout(() => {
+         setCarouselTransition("none");
+      }, 500);
+      setTimeout(() => {
+         setPickNumber(5);
+      }, 550);
+      setTimeout(() => {
+         setCarouselTransition("transform 0.5s ease-in-out");
+      }, 600);
+   } else setPickNumber(curNumber - 1);
+   setTimeout(() => {
+      setDisabled(false);
+   }, 500);
+};
+export const nextHandler: PageHandler = (argsArr) => {
+   const [pickNumber, setPickNumber, setCarouselTransition, setDisabled] =
+      argsArr;
+   const curNumber = pickNumber;
+   setDisabled(true);
+   if (curNumber >= 5) {
+      setPickNumber(6);
+      setTimeout(() => {
+         setCarouselTransition("none");
+      }, 500);
+      setTimeout(() => {
+         setPickNumber(1);
+      }, 550);
+      setTimeout(() => {
+         setCarouselTransition("transform 0.5s ease-in-out");
+      }, 600);
+   } else setPickNumber(curNumber + 1);
+   setTimeout(() => {
+      setDisabled(false);
+   }, 500);
+};
