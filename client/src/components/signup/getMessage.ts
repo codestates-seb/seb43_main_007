8ee473a7373df16f69f84750eb3e43806a29ceaf
kimsh322 <kimsh91322@gmail.com>
2023-05-14 20:10:02 +0900

    Fix : 회원가입 post response 처리함수 로직 수정

--- client/src/components/signup/getMessage.ts
+++ client/src/components/signup/getMessage.ts
@@ -12,42 +12,41 @@ interface GetMessage {
 }
 
 const getMessage: GetMessage = (response, setMessage, setIsModalOpen) => {
+   let modalMessage = { text1: "", text2: "" };
    switch (response) {
       case "success":
-         setMessage({
+         modalMessage = {
             text1: "회원가입에",
             text2: "성공하셨습니다.",
-         });
-         setIsModalOpen(true);
+         };
          break;
       case "Member exists":
-         setMessage({
+         modalMessage = {
             text1: "이미 존재하는",
             text2: "아이디입니다.",
-         });
-         setIsModalOpen(true);
+         };
          break;
       case "Member nickname already exists":
-         setMessage({
+         modalMessage = {
             text1: "이미 존재하는",
             text2: "닉네임입니다.",
-         });
-         setIsModalOpen(true);
+         };
          break;
       case "Password not same":
-         setMessage({
+         modalMessage = {
             text1: "비밀번호가",
             text2: "일치하지 않습니다.",
-         });
-         setIsModalOpen(true);
+         };
          break;
       default:
-         setMessage({
+         modalMessage = {
             text1: "서버 오류입니다.",
             text2: "다시 시도해 주세요.",
-         });
-         setIsModalOpen(true);
+         };
    }
+
+   setMessage(modalMessage);
+   setIsModalOpen(true);
 };
 
 export default getMessage;
