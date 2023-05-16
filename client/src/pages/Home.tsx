942aa29b231d916de6070825bc9c3575bf8bf93f
kimsh322 <kimsh91322@gmail.com>
2023-05-16 17:13:03 +0900

    Feat : home화면 배경 추가

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import HomeHeader from "../components/home/HomeHeader";
 import EditerPick from "../components/home/EditerPick";
+import background from "../assets/img/leaf.png";
 
 function Home() {
    return (
@@ -17,5 +18,18 @@ const HomeContainer = styled.div`
    flex-direction: column;
    align-items: center;
    width: 1080px;
-   background-color: aqua;
+   background-image: url(${background});
+   background-size: 1080px;
+   background-repeat: no-repeat;
+   // 배경 투명도 조절
+   &::before {
+      content: "";
+      opacity: 0.5;
+      position: absolute;
+      top: 0px;
+      left: 0px;
+      right: 0px;
+      bottom: 0px;
+      background-color: #fff;
+   }
 `;
