11294a1f450bd1f6f0fbbf85b225ce9707f81616
Hyojin Ju <hyojin.ju@gmx.net>
2023-05-17 23:21:32 +0900

    fix: img 본문 크기 넘어가는 부분 통제

--- client/src/components/postdetail/PostContent.tsx
+++ client/src/components/postdetail/PostContent.tsx
@@ -11,8 +11,9 @@ export default PostContent;
 
 export const PostContentContainer = styled.div`
    margin: 30px 0 30px 15px;
-   /* 아래 폰트 사이즈는 나중에 지울 것 */
-   p {
-      font-size: 13px;
+
+   img {
+      max-width: 100%;
+      height: auto;
    }
 `;
