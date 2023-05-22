82c099f965becb2e4ede425d05e4ac1ffdbd9338
kimsh322 <kimsh91322@gmail.com>
2023-05-22 20:13:40 +0900

    Feat : 북마크페이지 예외처리 추가

--- client/src/components/mypage-bookmark/BookmarkBox.tsx
+++ client/src/components/mypage-bookmark/BookmarkBox.tsx
@@ -11,6 +11,8 @@ function BookmarkBox() {
    const memberId = useSelector((state: RootState) => state.memberId);
    const [bookmarkBoardIds, setBookmarkBoardIds] = useState<number[]>([]);
    const [bookmarkItems, setBookmarkItems] = useState<BookmartItemType[]>([]);
+   const [errorMessage, setErrorMessage] =
+      useState("북마크된 글이 존재하지 않습니다.");
    // bookmark boardId 불러오기
    useEffect(() => {
       const fetchUserProfile = async () => {
@@ -20,7 +22,7 @@ function BookmarkBox() {
                setBookmarkBoardIds([...data.bookmarkBoardIds]);
             }
          } catch (error) {
-            console.error("실패", error);
+            setErrorMessage("서버 연결에 실패했습니다.");
          }
       };
       fetchUserProfile();
@@ -29,14 +31,18 @@ function BookmarkBox() {
    // 북마크 게시글 하나하나 불러오기
    useEffect(() => {
       const getBookmark = async (bookmarkIds: number[]) => {
-         const res = await Promise.all(
-            bookmarkIds.map((boardId) =>
-               request.get(`/boards/board`, {
-                  params: { memberId, boardId },
-               })
-            )
-         );
-         setBookmarkItems(res.map((el) => el.data));
+         try {
+            const res = await Promise.all(
+               bookmarkIds.map((boardId) =>
+                  request.get(`/boards/board`, {
+                     params: { memberId, boardId },
+                  })
+               )
+            );
+            setBookmarkItems(res.map((el) => el.data));
+         } catch (error) {
+            setErrorMessage("서버 연결에 실패했습니다.");
+         }
       };
       getBookmark(bookmarkBoardIds);
    }, [memberId, bookmarkBoardIds]);
@@ -52,9 +58,13 @@ function BookmarkBox() {
    );
    return (
       <BookmarkBoxContainer>
-         {currentItems.map((data) => {
-            return <BookmarkItem key={data.boardId} data={data} />;
-         })}
+         {totalDataCount > 0 ? (
+            currentItems.map((data) => {
+               return <BookmarkItem key={data.boardId} data={data} />;
+            })
+         ) : (
+            <div className="no-bookmark">{errorMessage}</div>
+         )}
          <BookmarkPagination
             totalDataCount={totalDataCount}
             currentPage={currentPage}
@@ -73,4 +83,12 @@ const BookmarkBoxContainer = styled.div`
    justify-content: center;
    padding-top: 5%;
    height: 1000px;
+   .no-bookmark {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 30%;
+      font-size: 25px;
+      color: var(--third-color3);
+   }
 `;
